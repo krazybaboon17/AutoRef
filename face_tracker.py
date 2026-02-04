4547841ae74b36e10b980d2f0e3faffede418df8
krazybaboon17 <krazybaboon17@gmail.com>
2026-02-04 23:46:35 +0000

    Face and Ball tracking on Web Server

--- face_tracker.py
+++ face_tracker.py
@@ -1,20 +1,40 @@
 import cv2
-from flask import Flask, Response
+from flask import Flask, Response, render_template
+import numpy as np
 
+lowerColor = np.array([5, 150, 150])
+upperColor = np.array([15, 255, 255])
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-videoCapture = cv2.VideoCapture(0)
+videoCapture = cv2.VideoCapture(1)
 
 app = Flask(__name__)
+scale = 0.5
 
 def generate_frames():
     while True:
         isRet, currentFrame = videoCapture.read()
         if not isRet:
             continue
-        grayImage = cv2.cvtColor(currentFrame, cv2.COLOR_BGR2GRAY)
+        resizedFrame = cv2.resize(currentFrame, None, fx=scale, fy=scale)
+        grayImage = cv2.cvtColor(resizedFrame, cv2.COLOR_BGR2GRAY)
         detectedFaces = faceCascade.detectMultiScale(grayImage, 1.1, 4)
         for (x, y, width, height) in detectedFaces:
-            cv2.rectangle(currentFrame, (x, y), (x+width, y+height), (255, 0, 0), 2)
+            x, y, w, h = int(x/scale), int(y/scale), int(width/scale), int(height/scale)
+            cv2.rectangle(currentFrame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.putText(currentFrame, "FACE", (x, y -10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        
+        blurredImg = cv2.GaussianBlur(currentFrame, (11, 11), 0)
+        hsvImage = cv2.cvtColor(blurredImg, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsvImage, lowerColor, upperColor)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if contours:                                           
+            c = max(contours, key=cv2.contourArea)            
+            x, y, w, h = cv2.boundingRect(c)                  
+            cv2.rectangle(currentFrame, (x, y), (x + w, y + h), (0, 0, 255), 2)  
+            cv2.putText(currentFrame, 'BALL', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)  
 
         success, encoded_image = cv2.imencode(".jpg", currentFrame)
         if not success:
@@ -23,10 +43,14 @@ def generate_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
